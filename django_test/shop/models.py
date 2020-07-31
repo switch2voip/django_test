e078a9da181c6e35f593e4b7a56d2ebf5f879e5e
switch2voip <matosvictor@gmail.com>
2020-07-31 10:14:03 -0400

    I have changed the settings

--- django_test/shop/models.py
+++ django_test/shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class form(models.Model):
+    id = models.AutoField
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20,default='')
+    email = models.CharField(max_length=25)
+    password = models.CharField(max_length=16)
+    Address = models.CharField(max_length=50)
+    Address2 = models.CharField(max_length=50,default='')
+    city = models.CharField(max_length=15)
+    state = models.CharField(max_length=25)
+    zip = models.CharField(max_length=15)
+
+

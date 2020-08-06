c76f1f7e66c021ed7a77111cbad3d957104a5fa5
switch2voip <matosvictor@gmail.com>
2020-08-06 10:01:27 -0400

    We have added some codes

--- django_test/shop/urls.py
+++ django_test/shop/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('rate', views.rate, name="rate"),
     path('services', views.services, name="services"),
-    path('myaccount', views.myaccount, name="myaccount")
+    path('myaccount', views.myaccount, name="myaccount"),
+    path('buyform', views.buyform, name="buyform")
     ]
--- django_test/shop/views.py
+++ django_test/shop/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 def home(request):
     return render(request,'shop/home.html')
 def rate(request):
-   return render(request,'rate.html')
+   return render(request,'shop/rate.html')
 def services(request):
-   return render(request,'services.html')
+   return render(request,'shop/services.html')
 def myaccount(request):
-   return render(request,'myaccount.html')
+   return render(request,'shop/myaccount.html')
+def buyform(request):
+    return render(request, 'shop/Buyform.html')
